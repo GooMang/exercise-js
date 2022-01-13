ce62989d54e659b87974393f4b8c834e57e0e1e8
Mankov Pavel <glouvarfre@gmail.com>
2022-01-13 12:43:17 +0700

    third exercise

--- js/3-chess-board.js
+++ js/3-chess-board.js
@@ -1,1 +1,10 @@
 console.log("  !3-chess-board!");
+for (let i = 1; i < 9; i++) {
+  for (let f = 1; f < 9; f++) {
+    if (i % 2 == 0) {
+      console.log(" # # # #");
+    } else {
+      console.log("# # # #");
+    }
+  }
+}
