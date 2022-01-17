eb12e524f0c8855ea30a177126ce4974e95b5d9b
Mankov Pavel <glouvarfre@gmail.com>
2022-01-17 22:02:46 +0700

    Add sum test

--- js/7-sumArray.test.js
+++ js/7-sumArray.test.js
@@ -1,11 +1,19 @@
-const { range } = require("./7-summArray");
+const { range, sum } = require("./7-summArray");
 
-test("default test", () => {
-  expect(range(2, 5)).toEqual([2, 3, 4, 5]);
+describe("range", () => {
+  test("default test", () => {
+    expect(range(2, 5)).toEqual([2, 3, 4, 5]);
+  });
+
+  test("with step", () => {
+    expect(range(1, 10, 2)).toEqual([1, 3, 5, 7, 9]);
+  });
 });
 
-test("with step", () => {
-  expect(range(1, 10, 2)).toEqual([1, 3, 5, 7, 9]);
+describe("sum", () => {
+  test("default test", () => {
+    expect(sum(range(1, 10))).toBe(55);
+  });
 });
 
 // test("with negative step", () => {
