a284a24a8e89ce23408864e7fadf477d73f76e9f
Mankov Pavel <glouvarfre@gmail.com>
2022-01-18 16:03:26 +0700

    3 rewriting

--- js/3-chess-board.js
+++ js/3-chess-board.js
@@ -1,17 +1,19 @@
 function exercise3() {
   console.log("  !3-chess-board!");
   console.log(getChessBoard());
-  // for (let i = 0; i < 8; i++) {
-  //   for (let j = 0; j < 8; j++) {
-  //     if (i % 2 == 0) {
-  //       console.log(" # # # #");
-  //     } else {
-  //       console.log("# # # #");
-  //     }
-  //   }
-  // }
 }
 
 function getChessBoard() {
-  return "# # # # \n # # # #"; // TODO
+  let board = "";
+  for (let i = 1; i < 9; i++) {
+    for (let j = 1; j < 9; j++) {
+      if ((i + j) % 2 == 0) {
+        board = board + "#";
+      } else {
+        board = board + " ";
+      }
+    }
+    board = board + "\n";
+  }
+  return board;
 }
