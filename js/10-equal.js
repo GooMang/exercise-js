7db682f493af130f740005b309f0ceca7ae371c0
Mankov Pavel <glouvarfre@gmail.com>
2022-03-14 21:38:53 +0700

    exercise 10 complet

--- js/10-equal.js
+++ js/10-equal.js
@@ -1,34 +1,43 @@
 function exercise10() {
-    console.log("  !10-list!");
-    const obj = { here: { is: "an" }, object: 2 };
-    const secObj = {};
-    for (let key in obj){
-
-    }
-    console.log(typeof secObj);
-    console.log(typeof null);
-    console.log(deepEqual(obj, obj));
-    
-console.log(deepEqual(obj, { here: 1, object: 2 }));
-console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
+  console.log("  !10-list!");
+  const obj = { here: { is: "an" }, object: 2 };
+  console.log(deepEqual(obj, obj));
+  console.log(deepEqual(obj, { here: 1, object: 2 }));
+  console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
 }
 
 function deepEqual(firstObj, secondObj) {
-
- if (typeof firstObj === typeof secObj && typeof firstObj === object){
-    for (let key in firstObj){
-        if (deepEqual(firstObj.key,secondObj.key) === false){
-            return false;
-        }
-    }
-    return true;
-       } 
-       
-      else if (firstObj === secondObj) {
-        return true;
-        break;
+  const zeroObj = {};
+  let firstObjCheck = 0;
+  let secondObjCheck = 0;
+  if (
+    typeof firstObj === typeof secondObj &&
+    typeof firstObj === typeof zeroObj
+  ) {
+    for (let key in firstObj) {
+      if (deepEqual(firstObj[key], secondObj[key]) === false) {
+        console.log("check-obj-no");
+        return false;
       }
-  else if (Number(firstObj) === Number(secondObj)){
-      return true;}
-
+      firstObjCheck++;
+    }
+    for (let key in secondObj) {
+      secondObjCheck++;
+    }
+    if (firstObjCheck == secondObjCheck) {
+      console.log("check-obj-yes");
+      return true;
+    } else {
+      return false;
+    }
+  } else if (firstObj === secondObj) {
+    console.log("check-three-=");
+    return true;
+  } else if (Number(firstObj) === Number(secondObj)) {
+    console.log("check-Number");
+    return true;
+  } else {
+    console.log("check-false");
+    return false;
+  }
 }
